2cbd4afe3601531c25bd5a03880b68e6aa42babd
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2014-11-27 15:00:29 +0100

    Adds PauseScript

---
+++ ConnaissanceDeSoi-Artisitque/Assets/Scripts/Common/PauseScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+    bool m_GamePaused;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            if (m_GamePaused) {
+                this.UnPause();
+            }
+            else {
+                this.Pause();
+            }
+        }
+    }
+
+    void Pause() {
+        Time.timeScale = 0.0f;
+        m_GamePaused = true;
+    }
+
+    void UnPause() {
+        Time.timeScale = 1.0f;
+        m_GamePaused = false;
+    }
+}
