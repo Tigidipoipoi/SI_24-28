f5fe79c1693277521509c9cc6da639f526637782
Adrien Peyromaure Debord Broca <a.peyromauredebordbroca@rubika-edu.com>
2014-11-28 09:58:40 +0100

    Adds menu

---
+++ ConnaissanceDeSoi-Artisitque/Assets/Scripts/Common/LaunchScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchScene : MonoBehaviour {
+
+
+	public void LoadScene(string sceneName)
+	{
+		Application.LoadLevel(sceneName);
+	}
+}
